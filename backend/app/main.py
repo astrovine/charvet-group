10974f003ef94f1140168eb073885c4ac8e1bac4
Uzoukwu Divine <divineuzoukwu3@gmail.com>
2025-10-13 23:55:08 +0100

    Refactor CORS middleware to use origins variable

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,21 +12,19 @@ app = FastAPI(
                 "copyright © 2025 Charvet Group. All rights reserved.",
     version="1.0.0"
 )
+origins = [
+    "https://charvet-group.vercel.app",
+    "http://localhost:3000",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://localhost:3001",
-        "https://*.vercel.app",
-        "https://charvet-group.vercel.app",
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(dashboard.router)
 app.include_router(user.router)
